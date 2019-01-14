5a88ba5ab66a25c7aedb82f2e63d2fc5cd5a5d84
Flavio Andrade <flavio.ssiv@gmail.com>
2019-01-14 16:25:48 -0300

    Iterators finished

--- ES6/dev/js/main.js
+++ ES6/dev/js/main.js
@@ -1,7 +1,39 @@
-var myName = "Sinner";
-
-var greetings = (name) => {
-  console.log(`Hellor there ${name}`);
+function moduleName(mdname) {
+  console.log(`- Content of the ${mdname} section`);
 }
 
-greetings(myName);
+moduleName("Iterators");
+
+
+let array = [1, 2, 3];
+
+let it = array[Symbol.iterator]();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+// Custom iterator
+let person = {
+  name: "Max",
+  hobbies: ['Sports', 'Cooking'],
+  [Symbol.iterator]: function () {
+    let i = 0;
+    let hobbies = this.hobbies;
+    return {
+      next: function () {
+        let value = hobbies[i];
+        i++;
+        return {
+          done: i > hobbies.length ? true : false,
+          value: value
+        }
+      }
+    }
+  }
+}
+
+for (let hobby of person) {
+  console.log(hobby);
+}
