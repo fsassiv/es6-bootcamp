77f4a5ab06397df683429cd849142d4e26039735
Flavio Andrade <flavio.ssiv@gmail.com>
2019-01-14 23:15:17 -0300

    Promises finished

--- ES6/dev/js/main.js
+++ ES6/dev/js/main.js
@@ -1,7 +1,50 @@
-var myName = "Sinner";
-
-var greetings = (name) => {
-  console.log(`Hellor there ${name}`);
+function moduleName(mdName) {
+  console.log(`- Content of the ${mdName} section`);
 }
 
-greetings(myName);
+moduleName("Promises");
+
+let promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve('Done!');
+    reject('Failed!');
+  }, 1500);
+});
+
+promise.then(function (value) {
+  console.log(value);
+}, function (error) {
+  console.log(error);
+});
+
+moduleName("Promises - Built-in methods");
+
+let promise1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve('Resolved!');
+  }, 1000);
+})
+
+
+let promise2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    reject('Rejected!');
+  }, 2000);
+})
+
+// Promise.all([promise1, promise2])
+//   .then(function (success) {
+//     console.log(success);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+
+Promise.race([promise1, promise2])
+  .then(function (success) {
+    console.log(success + ' race method');
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
