a0325556a1d4c89b54b2293f70b9bea272ecec27
Flavio Andrade <flavio.ssiv@gmail.com>
2019-01-16 11:52:11 -0300

    ojb ext finished

--- ES6/dev/js/main.js
+++ ES6/dev/js/main.js
@@ -2,49 +2,16 @@ function moduleName(mdName) {
   console.log(`- Content of the ${mdName} section`);
 }
 
-moduleName("Promises");
+moduleName("Object Extensions - Objects");
 
-let promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve('Done!');
-    reject('Failed!');
-  }, 1500);
-});
+var obj1 = {
+  a: 1
+}
 
-promise.then(function (value) {
-  console.log(value);
-}, function (error) {
-  console.log(error);
-});
+var obj2 = {
+  b: 2
+}
 
-moduleName("Promises - Built-in methods");
+var obj = Object.assign(obj1, obj2);
 
-let promise1 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    resolve('Resolved!');
-  }, 1000);
-})
-
-
-let promise2 = new Promise(function (resolve, reject) {
-  setTimeout(() => {
-    reject('Rejected!');
-  }, 2000);
-})
-
-// Promise.all([promise1, promise2])
-//   .then(function (success) {
-//     console.log(success);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-
-
-Promise.race([promise1, promise2])
-  .then(function (success) {
-    console.log(success + ' race method');
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+console.log(obj);
